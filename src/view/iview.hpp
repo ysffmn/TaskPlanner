c5c5c13cd084a2897c140b429000e79860328156
15446-rus75 <vladabramov06@yandex.ru>
2026-04-06 15:21:01 +0300

    update IView

--- src/view/iview.hpp
+++ src/view/iview.hpp
@@ -1,6 +1,8 @@
 #ifndef IVIEW_HPP
 #define IVIEW_HPP
 
+#include <QDate>
+#include <QList>
 #include <QObject>
 #include "../storage/task.hpp"
 
@@ -13,15 +15,21 @@ namespace view
   public:
     virtual ~IView() = default;
     virtual void showTaskList(const QList< storage::Task > &tasks) = 0;
-    virtual void showTasksForDate(const QDate &date, const QList< storage:: Task > &tasks) = 0;
+    virtual void showTasksForDate(const QDate &date, const QList< storage::Task > &tasks) = 0;
     virtual void showTaskCreationForm() = 0;
     virtual void showErrorMessage(const QString &message) = 0;
+    virtual void showInfoMessage(const QString &message) = 0;
 
   signals:
-    void taskAddedRequest(const storage::Task &task);
-    void taskDeletedRequest(int taskId);
-    void dateSelected(const QDate &date);
     void viewReady();
+    void taskAddRequested(const storage::Task &task);
+    void taskEditRequested(int taskID);
+    void taskUpdateRequested(const storage::Task &task);
+    void taskDeleteRequested(int taskId);
+    void taskCompleteRequested(int taskId);
+    void dateSelected(const QDate &date);
+    void sortRequested(storage::Criterion criterion);
+    void filterChanged(storage::Filter filter, const QVariant &value);
   };
 }
 
