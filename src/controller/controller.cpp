c584b08b7747941ba7c0bc56f2d5fda4859d0980
ysffmn <andrewnikonov387@gmail.com>
2026-04-09 02:28:15 +0300

    added skeleton controller.cpp

---
+++ src/controller/controller.cpp
@@ -0,0 +1,47 @@
+#include "controller.hpp"
+#include "../storage/istorage.hpp"
+
+controller::Controller::Controller():
+  m_storage(nullptr),
+  m_view(nullptr)
+{}
+
+void controller::Controller::setStorage(storage::IStorage* storage)
+{
+  m_storage = storage;
+}
+
+void controller::Controller::setView(view::IView* view)
+{
+  m_view = view;
+}
+
+void controller::Controller::start()
+{}
+
+void controller::Controller::onViewReady()
+{}
+
+void controller::Controller::onTaskAddRequested(const storage::Task& task)
+{}
+
+void controller::Controller::onTaskEditRequested(int taskId)
+{}
+
+void controller::Controller::onTaskUpdateRequested(const storage::Task& task)
+{}
+
+void controller::Controller::onTaskDeleteRequested(int taskId)
+{}
+
+void controller::Controller::onCompleteRequested(int taskId)
+{}
+
+void controller::Controller::onDateSelected(const QDate& date)
+{}
+
+void controller::Controller::onSortRequested(storage::Criterion criterion)
+{}
+
+void controller::Controller::onFilterChanged(storage::Filter filter, const QVariant &value)
+{}
