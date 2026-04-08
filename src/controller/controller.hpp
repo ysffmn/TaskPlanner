05966c67a1240044d7cbd3c9f45c45cdd9c7a86a
ysffmn <andrewnikonov387@gmail.com>
2026-04-09 02:17:13 +0300

    CG + default controller constructor fixes

--- src/controller/controller.hpp
+++ src/controller/controller.hpp
@@ -1,41 +1,38 @@
 #ifndef CONTROLLER_HPP
 #define CONTROLLER_HPP
 
-#include "IController.hpp"
 #include "../storage/istorage.hpp"
 #include "../view/iview.hpp"
+#include "icontroller.hpp"
 
 namespace controller
 {
-
-class Controller : public IController
-{
+  class Controller: public IController
+  {
     Q_OBJECT
 
-public:
-    explicit Controller(QObject* parent = nullptr);
+  public:
+    explicit Controller();
     ~Controller() override = default;
 
-    void setStorage(storage::IStorage* storage) override;
-    void setView(view::IView* view) override;
+    void setStorage(storage::IStorage *storage) override;
+    void setView(view::IView *view) override;
     void start() override;
 
-public slots:
+  public slots:
     void onViewReady() override;
-    void onTaskAddRequested(const storage::Task& task) override;
+    void onTaskAddRequested(const storage::Task &task) override;
     void onTaskEditRequested(int taskId) override;
-    void onTaskUpdateRequested(const storage::Task& task) override;
+    void onTaskUpdateRequested(const storage::Task &task) override;
     void onTaskDeleteRequested(int taskId) override;
     void onCompleteRequested(int taskId) override;
     void onDateSelected(const QDate &date) override;
     void onSortRequested(storage::Criterion criterion) override;
-    void onFilterChanged(storage::Filter filter, const QVariant& value) override;
+    void onFilterChanged(storage::Filter filter, const QVariant &value) override;
 
-private:
-    storage::IStorage* m_storage = nullptr;
-    view::IView* m_view = nullptr;
-};
-
+  private:
+    storage::IStorage *m_storage = nullptr;
+    view::IView *m_view = nullptr;
+  };
 }
-
 #endif
