2663f0813b412c9a9b7315300730006d2f411c14
ysffmn <andrewnikonov387@gmail.com>
2026-04-09 17:07:21 +0300

    nullptr checks added

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -6,29 +6,47 @@ controller::Controller::Controller(QObject *parent):
   m_view(nullptr)
 {}
 
-void controller::Controller::setStorage(storage::IStorage* storage)
+void controller::Controller::setStorage(storage::IStorage *storage)
 {
+  if (!storage)
+  {
+    qWarning() << "Controller::setStorage: storage pointer is nullptr";
+    return;
+  }
   m_storage = storage;
 }
 
-void controller::Controller::setView(view::IView* view)
+void controller::Controller::setView(view::IView *view)
 {
+  if (!view)
+  {
+    qWarning() << "Controller::SetView: view pointer is nullptr";
+    return;
+  }
   m_view = view;
 }
 
 void controller::Controller::start()
-{}
+{
+  if (!m_storage || !m_view)
+  {
+    qCritical() << "Controller::start: storage or view module is not provided";
+    qCritical() << "Storage status: " << (m_storage ? "ok" : "nullptr");
+    qCritical() << "View status: " << (m_view ? "ok" : "nullptr");
+    return;
+  }
+}
 
 void controller::Controller::onViewReady()
 {}
 
-void controller::Controller::onTaskAddRequested(const storage::Task& task)
+void controller::Controller::onTaskAddRequested(const storage::Task &task)
 {}
 
 void controller::Controller::onTaskEditRequested(int taskId)
 {}
 
-void controller::Controller::onTaskUpdateRequested(const storage::Task& task)
+void controller::Controller::onTaskUpdateRequested(const storage::Task &task)
 {}
 
 void controller::Controller::onTaskDeleteRequested(int taskId)
@@ -37,7 +55,7 @@ void controller::Controller::onTaskDeleteRequested(int taskId)
 void controller::Controller::onCompleteRequested(int taskId)
 {}
 
-void controller::Controller::onDateSelected(const QDate& date)
+void controller::Controller::onDateSelected(const QDate &date)
 {}
 
 void controller::Controller::onSortRequested(storage::Criterion criterion)
